f4c3e398be7752cb63871b59eebaf0255812ec67
ALCM-bit <falcm-bit@outlook.com>
2025-01-18 10:56:22 -0300

    Arquivos iniciais Terraform

---
+++ terraform/provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.62.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "github-pipe"
+    key    = "hackathon-api/terraform.tfstate"
+    region = "sa-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Project = "Infra para projeto processa vídeo"
+      Owner   = "Grupo 28"
+    }
+  }
+}
