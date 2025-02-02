4619a90b32110833b1871c34e3a901fc53e0b7b6
ALCM-bit <falcm-bit@outlook.com>
2025-02-01 22:44:45 -0300

    Configurações iniciais terraform

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,46 +1,83 @@
+# VPC
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   tags = {
-    "name" = "main"
+    Name = "main"
   }
 }
 
+# Subnets
 resource "aws_subnet" "subnet" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.1.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
 }
 
 resource "aws_subnet" "subnet2" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.2.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
 }
 
+# Internet Gateway
 resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = aws_vpc.main.id
   tags = {
-    name = "internet_gateway"
+    Name = "internet_gateway"
   }
 }
 
+# Route Table
 resource "aws_route_table" "route_table" {
   vpc_id = aws_vpc.main.id
-  route = {
+
+  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.internet_gateway.id
   }
+
+  tags = {
+    Name = "route_table"
+  }
 }
 
+# Route Table Associations
 resource "aws_route_table_association" "subnet_route" {
-    subnet_id = aws_subnet.subnet.id
-    route_table_id = aws_route_table.route_table.id  
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route_table.id
 }
 
 resource "aws_route_table_association" "subnet2_route" {
-    subnet_id = aws_subnet.subnet2.id
-    route_table_id = aws_route_table.route_table.id  
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+# Security Group
+resource "aws_security_group" "security_group" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # -1 significa "todos os protocolos"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "any"
+    self        = false
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "any"
+    self        = false
+  }
+
+  tags = {
+    Name = "security_group"
+  }
 }
